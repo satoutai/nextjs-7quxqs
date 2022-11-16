42a308f52cc250c6dcdb424fa090431b761891ba
satoutai <118322639+satoutai@users.noreply.github.com>
2022-11-16 11:34:10 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# nextjs-7quxqs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/nextjs-7quxqs)
---
+++ pages/sample.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+function Sample() {
+  return <span>サンプルページです。</span>;
+}
+
+export default Sample;
