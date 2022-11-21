4cbcea56aa1ba6ff5e72794d6a6cbdcea4636ba9
satoutai <118322639+satoutai@users.noreply.github.com>
2022-11-21 14:33:44 +0900

    1 file added

---
+++ pages/isr.tsx
@@ -0,0 +1,46 @@
+import { GetStaticPaths, NextPage, GetStaticProps } from 'next';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
+
+type ISRProps = {
+  message: string;
+};
+
+// ISRPropsを受け付けるNextPage（ページ）の型
+const ISR: NextPage<ISRProps> = (props) => {
+  const { message } = props;
+  const router = useRouter();
+
+  if (router.isFallback) {
+    //フォールバック用のページを返す。
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Create Next App</title>
+        <link rel="icon" href="/test.icon" />
+      </Head>
+      <main>
+        <p>このページは、ISRによってビルド時に生成されたページです。</p>
+        <p>{message}</p>
+      </main>
+    </div>
+  );
+};
+
+export const getStaticProps: GetStaticProps<ISRProps> = async (context) => {
+  const timestamp = new Date().toLocaleString();
+  const message = `${timestamp}にこのページのgetStaticPropsに実行されました。`;
+
+  return {
+    props: {
+      message,
+    },
+    //ページの有効期限
+    revalidate: 60,
+  };
+};
+
+export default ISR;
