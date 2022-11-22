498abaeb320b1fb62d4aff827718ac794dd8953a
satoutai <118322639+satoutai@users.noreply.github.com>
2022-11-22 10:36:09 +0900

    1 file modified

--- pages/image-sample.tsx
+++ pages/image-sample.tsx
@@ -1,3 +1,20 @@
 import { NextPage } from 'next';
 import Image from 'next/image';
-//画像をインポートする。
+// import to Image
+import BibleImage from '../public/images/bible.png';
+
+const ImageSample: NextPage<void> = (props) => {
+  return (
+    <div>
+      <h1>画像表示の比較</h1>
+      <p>imgタグで表示した場合</p>
+      {/* 通常のimgタグを使用して画像を表示 */}
+      <img src="/images/bible.png" />
+      <p>Imageコンポーネントで表示した場合</p>
+      {/* Imageコンポーネントで表示した場合 */}
+      {/* パスを指定する代わりに、インポートした画像を指定 */}
+      <Image src={BibleImage} />
+      <p>Imageで表示した場合は事前に描画エリアが確保されます。</p>
+    </div>
+  );
+};
